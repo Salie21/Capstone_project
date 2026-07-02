3687cf56c7c38801ea4b10cde74d47235ce4f232
Ridwaan <125797710+Salie21@users.noreply.github.com>
2026-07-02 19:50:21 +0200

    Update main.jsx

--- Frontend/Airbnb-Project/src/main.jsx
+++ Frontend/Airbnb-Project/src/main.jsx
@@ -1,3 +1,7 @@
+/* Import StrictMode application into the root HTML element
+Imported React DOM to render the application
+Imported global styles
+*/
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
@@ -5,7 +9,7 @@ import { AuthProvider } from "./context/AuthContext"
 import App from "./App.jsx"
 import "./index.css"
 
-
+// Render the React application into the root HTML element
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
