99023e1c9f4cbaadc32168700e62f56b846f2e22
Ridwaan <125797710+Salie21@users.noreply.github.com>
2026-07-02 18:26:04 +0200

    Update reservationRoutes.js

--- Backend/Routes/reservationRoutes.js
+++ Backend/Routes/reservationRoutes.js
@@ -1,15 +1,20 @@
+/* Import the Express framework and create a new router
+Import the auth controller and asweel for middleware for authentication and role-based authorization */
 const express = require("express");
 const router = express.Router();
 const reservationController = require("../Controllers/ReservationController");
 const auth = require("../Middleware/auth");
 const role = require("../Middleware/role");
 
+//Uses CRUD operations for reservations
 router.post("/", auth, reservationController.createReservation);
 
+//Read reservation
 router.get("/host", auth, role(["host"]), reservationController.getHostReservations);
 
 router.get("/user", auth, reservationController.getUserReservations);
 
+//Deletes reservation 
 router.delete("/:id", auth, reservationController.deleteReservation);
 
 module.exports = router;
