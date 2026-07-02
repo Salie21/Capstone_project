f261def1f8f6df6ba73fdc64be6acaa7cfcc580f
Ridwaan <125797710+Salie21@users.noreply.github.com>
2026-07-02 19:27:04 +0200

    Update Discover.jsx

--- Frontend/Airbnb-Project/src/Components/Discover.jsx
+++ Frontend/Airbnb-Project/src/Components/Discover.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+
+//Imports styling for the page 
 import './Discover.css'
 
-
+// Defines Discover as a variable
 const Discover = () => {
   return (
     <section className='discover_page'>
       <h2 className="discover_heading">Discover Airbnb Experiences</h2>
-
+      
+//Card on the right, static with button
       <div className='discover_card'>
         <h3>Things to do on your trip</h3>
         <button>Experiences</button>
       </div>
-
+//Card on the left, static with button
       <div className='discover_Container'>
         <h3>Things to do from home</h3>
         <button>Online Experiences</button>
