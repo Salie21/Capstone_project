0a6f3bae732c7924d0d99ea675302ea915fd5565
Ridwaan <125797710+Salie21@users.noreply.github.com>
2026-07-02 18:27:46 +0200

    Update userRoutes.js

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -1,6 +1,12 @@
+/* Import the Express framework and create a new router
+Import the controller */
 const express = require("express");
 const router = express.Router();
 const userController = require("../Controllers/UserController");
+
+/*Receives the user's username and password.
+Checks if the username already exists.
+Hashes the password using bcrypt.*/ 
 
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
