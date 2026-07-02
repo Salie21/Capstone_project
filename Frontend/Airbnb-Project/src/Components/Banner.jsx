bf2e0fcf8ab7f838fd8b06e49bee1b5a39b26dc9
Ridwaan <125797710+Salie21@users.noreply.github.com>
2026-07-02 19:23:39 +0200

    Update Banner.jsx

--- Frontend/Airbnb-Project/src/Components/Banner.jsx
+++ Frontend/Airbnb-Project/src/Components/Banner.jsx
@@ -1,15 +1,21 @@
+// Import React and the useState hook for managing component state
 import React, { useState } from 'react'
+
+//Import styling sheet for the bannerr
 import './Banner.css'; 
+
+//Import mui-icon
 import Button from '@mui/material/Button';
 
 
-
+//state variable called showSearch that controls whether the search bar (or search section) is visible. 
 const Banner = () => {
        const [showSearch, setShowSearch] = useState(false);
   return (
     <div className='banner'>
         <div className='banner_search'> {showSearch && <h1>Show date picker</h1>}
-
+               
+//Mui icon used for banner button 
         <p className='banner_info'>Not sure where to go? Perfect.</p>
         <Button className='banner_button'>I'm flexible</Button>
 
